ef84f08a926a039882c97a7abd0c387b03a48918
fujitatks <t.fujita.since.19820819@gmail.com>
2018-12-19 13:52:36 +0000

    scraping practice finish

--- scraping.rb
+++ scraping.rb
@@ -1,8 +1,32 @@
 require 'mechanize'
 
 agent = Mechanize.new
-page = agent.get("https://app-mooovi.herokuapp.com/works/initial_scraping")
-elements = page.search('p')
+# page = agent.get("https://app-mooovi.herokuapp.com/works/initial_scraping")
+# elements = page.search('p')
+
+# elements.each do |ele|
+#   puts ele.inner_text
+# end
+
+#練習問題１
+# page = agent.get("https://app-mooovi.herokuapp.com/works/first_scraping")
+# elements = page.search('li')
+
+# elements.each do |ele|
+#   puts ele.inner_text
+# end
+
+#練習問題２
+# page = agent.get("https://app-mooovi.herokuapp.com/works/second_scraping")
+# elements = page.search('div div')
+
+# elements.each do |ele|
+#   puts ele.inner_text
+# end
+
+#練習問題３
+page = agent.get("https://app-mooovi.herokuapp.com/works/third_scraping")
+elements = page.search('div.etc div')
 
 elements.each do |ele|
   puts ele.inner_text
