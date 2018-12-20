a9a005456b5faee9f6fb0e52a258fd260858a362
fujitatks <t.fujita.since.19820819@gmail.com>
2018-12-20 07:23:17 +0000

    get bigger image

--- scraping_image.rb
+++ scraping_image.rb
@@ -1,10 +1,21 @@
 require 'mechanize'
 
 agent = Mechanize.new
-page = agent.get("http://review-movie.herokuapp.com/")
+pagen1 = "http://review-movie.herokuapp.com"
+page = agent.get(pagen1)
 
-elements = page.search('img.mini-post-image')
+elements = page.search('h2.entry-title a')
 
 elements.each do |ele|
-  puts ele.get_attribute('src')
+  pagen2 = ele.get_attribute('href')
+  # p pagen1+pagen2
+  page2 = agent.get(pagen1+pagen2)
+  
+  elements2 = page2.search('img.post-image')
+  # p elements2
+  # puts elements2.get_attribute('src')
+  elements2.each do |ele2|
+    # puts ele2    
+    puts ele2.get_attribute('src')
+  end
 end
